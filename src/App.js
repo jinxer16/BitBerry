03ce6aaa675b149025cb21572d3d16b6f867e54d
adeelahmed057 <adeelnazir2018@gmail.com>
2022-11-14 16:05:11 +0500

    4pm

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Home/header/header";
 import Footer from './Home/footer/footer'
 import Slider from "./Home/slider/Slider";
+import Universe from "./Home/nxtUniverse/Universe";
+import Carousel from "./Home/carousel/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Slider/>
+      <Carousel/>
+      <Universe/>
       <Footer />
     </div>
   );
---
+++ src/Home/nxtUniverse/Universe.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Universe.css'
+
+function Universe() {
+  return (
+    <>
+        <div className='row  d-flex bg'>
+        <div className='col-md-12'>
+        <div className=' d-flex justify-content-flex-end box'>
+
+        </div>
+
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Universe
