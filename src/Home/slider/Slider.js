e28d6710176e29db60055f9eca5a28f26ad55fa9
adeelahmed057 <adeelnazir2018@gmail.com>
2022-11-14 13:15:53 +0500

    change

--- src/Home/slider/Slider.js
+++ src/Home/slider/Slider.js
@@ -4,11 +4,10 @@ import "./Slider.css";
 function Slider() {
   return (
     <>
-      <div className="col-md-12">
-        <div className="txt-bg">WEBTOON UNIVERSE</div>
-      </div>
-      <div>
-        
+      <div className="row">
+        <div className="col-md-12">
+          <div className="txt-bg">WEBTOON UNIVERSE</div>
+        </div>
       </div>
     </>
   );
