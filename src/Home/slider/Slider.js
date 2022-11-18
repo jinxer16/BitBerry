3342de5c87ae9b38f53a7ccf85b42a0f3053e966
Faheem <mcsf19a010@pucit.edu.pk>
2022-11-18 14:35:24 +0500

    changes

--- src/Home/slider/Slider.js
+++ src/Home/slider/Slider.js
@@ -4,17 +4,18 @@ import "./Slider.css";
 function Slider() {
   return (
     <>
-    <div className="sliderContainer">
-    <div class="col-md-12 Marquee">
-  <div class="Marquee-content">
-    <div class="Marquee-tag ">WEBTOON UNIVERSE</div>
-    {/* <div class="Marquee-tag-2 ">WEBTOON UNIVERSE</div> */}
-    {/* <div class="Marquee-tag ">WEBTOON UNIVERSE</div>
-    <div class="Marquee-tag-2">WEBTOON UNIVERSE</div> */}
-  </div>
-</div>
-    </div>
-      
+      <div className="sliderContainer">
+        <div className="row">
+          <div class="col-md-12 Marquee">
+            <div class="Marquee-content">
+              <div class="Marquee-tag">WEBTOON UNIVERSE</div>
+              <div class="Marquee-tag-2">WEBTOON UNIVERSE</div>
+              <div class="Marquee-tag">WEBTOON UNIVERSE</div>
+              <div class="Marquee-tag-2">WEBTOON UNIVERSE</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
